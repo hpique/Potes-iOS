ba3c2db89b42a94722b87c4aac12a7d39c04cc7e
Hermes Pique <hermes.pique@robotmedia.net>
2012-02-19 09:34:10 +0100

    Initial Commit

---
+++ Potes/POAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  POAppDelegate.h
+//  Potes
+//
+//  Created by Hermes Pique on 2/19/12.
+//  Copyright (c) 2012 Robot Media SL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface POAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Potes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Potes
+//
+//  Created by Hermes Pique on 2/19/12.
+//  Copyright (c) 2012 Robot Media SL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "POAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([POAppDelegate class]));
+    }
+}
